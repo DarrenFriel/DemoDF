67ef1f3a3b421ef33188abf204e446a603d70ac8
Darren <darrenbfriel@gmail.com>
2025-11-19 12:18:40 +0000

    Add files via upload

--- calcTest.py
+++ calcTest.py
@@ -8,6 +8,10 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(5, calc.add(2, 3))
         self.assertEqual(0, calc.add(-2, 2))
         self.assertEqual(-5, calc.add(-2, -3))
-
+    def test_sub(self):
+        calc = BasicCalc()
+        self.assertEqual(1, calc.sub(3, 2))
+        self.assertEqual(-3, calc.sub(-1, 2))
+        self.assertEqual(1, calc.sub(-2, -3))
 if __name__ == '__main__':
     unittest.main()
